79ba2ba537e1feb908edf4da42f566d528f6aa05
Abhay <abhaykv04@gmail.com>
2021-10-07 18:30:05 +0530

    Refactor code

--- src/App.js
+++ src/App.js
@@ -8,66 +8,63 @@ import SideBar from './components/SideBar';
 import Graph from './components/Graph';
 
 const App = () => {
-    const [state, setState] = useState({
-        cities: {},
-        selectedCity: ''
-    });
+    const [ws, setWs] = useState(null);
+    const [state, setState] = useState({ cities: {}, selectedCity: '' });
+
+    // setup web socket on mount
+    useEffect(() => {
+        setWs(new WebSocket('ws://city-ws.herokuapp.com'));
+    }, []);
 
     useEffect(() => {
-        const ws = new WebSocket('ws://city-ws.herokuapp.com');
+        // setup onmessage listener when socket connection is available
+        if (ws !== null) {
+            ws.onmessage = (event) => {
+                const data = JSON.parse(event.data);
+                const newState = cloneDeep(state);
 
-        ws.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            const newState = cloneDeep(state);
+                // set first city in first message as default selected city
+                if (isEmpty(newState.selectedCity)) {
+                    newState.selectedCity = data[0].city;
+                }
 
-            // set first city in first message as default selected city
-            if (isEmpty(newState.selectedCity)) {
-                newState.selectedCity = data[0].city;
-            }
-
-            // construct data to be used in sidebar and graph
-            // pair = { city: <city-name>, aqi: <current-aqi> }
-            forEach(data, (pair) => {
-                if (has(newState.cities, pair.city)) {
-                    newState.cities[pair.city].push({
-                        aqi: pair.aqi.toFixed(2),
-                        updated: new Date().toLocaleTimeString()
-                    });
-                } else {
-                    newState.cities[pair.city] = [
-                        {
+                // construct data to be used in sidebar and graph
+                // pair = { city: <city-name>, aqi: <current-aqi> }
+                forEach(data, (pair) => {
+                    if (has(newState.cities, pair.city)) {
+                        newState.cities[pair.city].push({
                             aqi: pair.aqi.toFixed(2),
                             updated: new Date().toLocaleTimeString()
-                        }
-                    ];
-                }
-            });
+                        });
+                    } else {
+                        newState.cities[pair.city] = [
+                            {
+                                aqi: pair.aqi.toFixed(2),
+                                updated: new Date().toLocaleTimeString()
+                            }
+                        ];
+                    }
+                });
 
-            setState(newState);
-        };
-
-        return () => ws.close();
-    }, [state]);
+                setState(newState);
+            };
+        }
+    }, [ws, state]);
 
     /**
      * Handles updating state with selected city
      * @param {string} city - name of the city selected in sidebar
      */
     const handleCityClick = (city) => {
-        setState({
-            ...state,
-            selectedCity: city
-        });
+        setState({ ...state, selectedCity: city });
     };
 
     /**
      * Handles updating state with selectedCity as 'all'
      */
     const handleCompareAllClick = () => {
-        setState({
-            ...state,
-            selectedCity: 'all'
-        });
+        // TBI: Compare All functionality
+        // setState({ ...state, selectedCity: 'all' });
     };
 
     return (
@@ -80,7 +77,7 @@ const App = () => {
                     className='col-4'
                     style={{
                         height: 'calc(100vh - 50px)',
-                        'overflow-y': 'auto'
+                        overflowY: 'auto'
                     }}
                 >
                     <SideBar
